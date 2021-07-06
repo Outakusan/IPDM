a4f81058961125972fb972a4e9bb35bf9ecab69d
outakusan <outakuakagami@gmail.com>
2021-07-06 07:04:33 +0200

    IPDM

---
+++ IPDM.py
@@ -0,0 +1,106 @@
+import os
+import sys
+os.system("pip3 install socket")
+os.system("pip install socket")
+os.system("apt-get install python")
+os.system("apt-get install python3")
+os.system("apt-get install nmap")
+os.system("clear")
+print("""
+                             +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                             +                                                    +  Name Tool : IP Drak Mode        +
+                             + 11010010101001010     11001010010101100101011      +  Country   : Maroc               +
+                             + 00100101010100111     10010100110101001010101      +  Github    : ???                 +
+                             +     010101001         010010101        1010010     +  Tool work : install basic ...   +
+                             +     101010010         011100011        0001010     +  Version   : 1.0                 +
+                             +     100101010         011101110        010100      +  Year      : ???                 +
+                             +     100100001         010101110       010101       +  For       : Anybody             +
+                             +     100101011         001110010010100101011        + ++++++++++++++++++++++++++++++++ +
+                             +     101001001         1111001111001010110          +  Tip:                            +
+                             +     101001010         101010101|                   +   This Tool For Install basic    +
+                             +  0001011010101101  0100101010100111                +   Command of Linux and give you  +
+                             +  0101100011100101  0101010010100110                +   IP address of Anywebsite and   +
+                             +       ++++++++++++++++++++++++                     +   Can by This Tool install some  +
+                             +          Sacn of Drak Mode                         +   Tool .                         +
+                             ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++     ++++++++
+                                        +++++++++++++++++++++++++++++++++++++++ +++++++++++++++++
+                                        + 1 - IP Address Of website                             +
+                                        + 2 - Maker File .Sh for Install basic Command Linux.   +
+                                        + 3 - Install some tool by IP Drak Mode                 +              
+                                        +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                     | You install this Tool so you have a namp tool and Tor script |
+                                             
+                                                       By : [MarocBlackShinigami]""")
+input("{-1} | IPDM ...")
+f = "|"
+for x in range(100+1):
+ print("[",x,"]","|"*x)
+# here i want think user this Tool
+
+think = input("Do You Want Clear Terminal/console Y/N: ")
+if think == "Y" and "y":
+ os.system("clear")
+ print("Your wlcome to IPDM Tool")
+else :
+ print("You Don't Want Clear Terminal")
+# python3 scan
+started = input("@linuxSystem\root\IPDM : ")
+if started == "1" :
+ io=input("@linuxSystem\rootIPDM : ")
+ import socket
+ ip=socket.gethostbyname(io)
+ print("""
+             [info this web ]
+             =================================================
+             =  Your Url             ==     Ip               =
+             =================================================
+             =   """,io,"""          == """,ip,"""           =
+             =                       ==                      =
+             =================================================
+             Table Info of:""",io,"""""")
+if started == "3":
+ input("You Want Install Some Tool of hacking...")
+ os.system("apt-get install python")
+ os.system("apt-get install python2")
+ os.system("apt-get install python3")
+ os.system("apt-get install tor")
+ os.system("apt-get install ruby")
+ os.system("apt-get install perl")
+ os.system("apt-get install php")
+ os.system("apt-get install nano")
+ print("+")
+ print("Now you install tool hacking facebook")
+ print("+")
+ os.system("git clone https://github.com/FR13ND8/BRUTEFORCEnew")
+ print("Install is good")
+ os.system("git clone https://github.com/mkdirlove/FBTOOL") 
+ os.system("git clone https://github.com/ayoubsiray/gmail_attacker.git")
+ os.system("apt-get install metasploit")
+ os.system("clear")
+ os.system("chmod +x *")
+if started == "2":
+ print("this option is not found but we can install this commnad of linux ")
+ import os
+ os.system("apt-get update")
+ os.system("apt-get upgrade")
+ os.system("apt-get install python")
+ os.system("apt-get install python2")
+ os.system("apt-get install python3")
+ os.system("apt-get install ruby")
+ os.system("apt-get install nano")
+ os.system("apt-get install pico")
+ os.system("apt-get install perl")
+ os.system("apt-get install tor")
+ os.system("apt-get install *")
+ os.system("apt-get install php")
+ os.system("apt-get install sl")
+ os.system("apt-get install google")
+ os.system("apt-get install git")
+ os.system("apt-get install clone")
+ os.system("apt-get install chmod")
+ os.system("apt-get install pip")
+ os.system("apt-get install namp")
+else:
+  exit
+exit
+ 
